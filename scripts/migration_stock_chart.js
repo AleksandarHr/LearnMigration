353d58e8cadd8de1e388ef8729591a5b38b84a94
Aleksandar Hrusanov <hrusanov@grinnell.edu>
2020-05-20 16:29:28 +0300

    Working on dropdown menus update

--- scripts/migration_stock_chart.js
+++ scripts/migration_stock_chart.js
@@ -7,9 +7,9 @@ class MigrationStockChart {
     constructor(element_id, stock_data, chosen_country, chosen_year) {
 
         // set svg's height and with
-        this.width = 400;
+        this.width = 500;
         this.bar_height = 20;
-        this.height = 16 * 20; // # age groups * 20
+        this.height = 15 * this.bar_height; // # age groups * 20
         this.chosen_country = chosen_country;
         this.chosen_year = chosen_year;
         this.stock_data = stock_data;
@@ -29,65 +29,55 @@ class MigrationStockChart {
                 female: this.female_stock_numbers[i]
             };
         });
-        console.log(this.chart_data)
 
         // set margins
         this.margin = {
-            top: 50,
-            left: 50,
-            right: 50,
-            bottom: 50
+            top: 20,
+            left: 20,
+            right: 20,
+            bottom: 20
         };
 
+        var labelArea = 80;
         // set actual height and width (remove margins)
         this.SVG_HEIGHT = this.height + this.margin.top + this.margin.bottom;
-        this.SVG_WIDTH = this.width + this.margin.left + this.margin.right;
-
-        var chart = d3.select('#' + element_id)
-
-        var labelArea = 160;
-
-        // var chart,
-        //     width = 400,
-        //     bar_height = 20,
-        //     height = bar_height * (names.length);
+        this.SVG_WIDTH = 2 * this.width + labelArea + this.margin.left + this.margin.right;
 
         var rightOffset = this.width + labelArea;
-
         var chart = d3.select('#' + element_id)
             .append('svg')
             .attr('class', 'chart')
-            .attr('width', labelArea + this.width + this.width)
-            .attr('height', this.height);
+            .attr("viewBox", `0 0 ${this.SVG_WIDTH} ${this.SVG_HEIGHT}`)
 
-        var xFrom = d3.scale.linear()
+        var me = this;
+        var xLeft = d3.scale.linear()
             .domain([0, d3.max(this.male_stock_numbers)])
-            .range([0, this.width]);
+            .range([0, me.width]);
 
         var y = d3.scale.ordinal()
             .domain(this.age_groups)
-            .rangeBands([10, this.height]);
+            .rangeBands([0, me.height]);
 
         var yPosByIndex = function(d, index) {
             return y.range()[index];
         }
 
-        chart.selectAll("rect.left")
+        chart.selectAll("rect.male")
             .data(this.male_stock_numbers)
             .enter().append("rect")
-            .attr("x", function(pos) {
-                return this.width - xFrom(pos);
+            .attr("x", function(d) {
+                return me.width - xLeft(d) - 15;
             })
             .attr("y", yPosByIndex)
-            .attr("class", "left")
-            .attr("width", xFrom)
+            .attr("class", "male")
+            .attr("width", xLeft)
             .attr("height", y.rangeBand());
 
         chart.selectAll("text.leftscore")
             .data(this.male_stock_numbers)
             .enter().append("text")
             .attr("x", function(d) {
-                return this.width - xFrom(d);
+                return me.width - xLeft(d);
             })
             .attr("y", function(d, z) {
                 return yPosByIndex(d, z) + y.rangeBand() / 2;
@@ -101,8 +91,8 @@ class MigrationStockChart {
         chart.selectAll("text.name")
             .data(this.age_groups)
             .enter().append("text")
-            .attr("x", (labelArea / 2) + this.width)
-            .attr("y", function(d) {
+            .attr("x", (labelArea / 2) + me.width)
+            .attr("y", function(d, index) {
                 return y(d) + y.rangeBand() / 2;
             })
             .attr("dy", ".20em")
@@ -110,24 +100,24 @@ class MigrationStockChart {
             .attr('class', 'name')
             .text(String);
 
-        var xTo = d3.scale.linear()
+        var xRight = d3.scale.linear()
             .domain([0, d3.max(this.female_stock_numbers)])
-            .range([0, this.width]);
+            .range([0, me.width]);
 
-        chart.selectAll("rect.right")
+        chart.selectAll("rect.female")
             .data(this.female_stock_numbers)
             .enter().append("rect")
             .attr("x", rightOffset)
             .attr("y", yPosByIndex)
-            .attr("class", "right")
-            .attr("width", xTo)
+            .attr("class", "female")
+            .attr("width", xRight)
             .attr("height", y.rangeBand());
 
         chart.selectAll("text.score")
             .data(this.female_stock_numbers)
             .enter().append("text")
             .attr("x", function(d) {
-                return xTo(d) + rightOffset;
+                return xRight(d) + rightOffset;
             })
             .attr("y", function(d, z) {
                 return yPosByIndex(d, z) + y.rangeBand() / 2;
@@ -137,6 +127,14 @@ class MigrationStockChart {
             .attr("text-anchor", "end")
             .attr('class', 'score')
             .text(String);
+    }
+
+    setYear(selected_year) {
+      this.chosen_year = selected_year
+    }
+
+    setCountry(selected_country){
+      this.chosen_country = selected_country
     }
 }
 
@@ -155,42 +153,71 @@ function ready(error, stock_data) {
         console.log("Error loading data: " + error);
         throw error;
     }
-    // this.prep();
-    var generateChartButton = document.getElementById("generate_chart_button");
-    generateChartButton.addEventListener('click', function() {
-        var chosen_country = "Austria";
-        var chosen_year = 2010;
-        migrationStockChart = new MigrationStockChart("migration_stock_chart", stock_data, chosen_country, chosen_year);
-    });
+    var default_year = 1990
+    var default_country = "Austria"
+    var migrationStockChart = new MigrationStockChart("migration_stock_chart", stock_data, default_country, default_year);
+    selectParameters(stock_data, migrationStockChart);
 }
 
-// function prep() {
-//   // dc.js chart types
-//   let countrySelect = dc.selectMenu('#countries_bar_chart');
-//   let yearBarChartSelect = dc.selectMenu('#years_bar_chart');
-//
-//   countrySelect.on('pretransition', function(chart) {
-//       // add styling to select input
-//       d3.select('#countries_bar_chart').classed('dc-chart', false);
-//       // use Bootstrap styling
-//       chart.select('select').classed('form-control', true);
-//   });
-//
-//   countrySelect.on('filtered', function(chart, filter) {
-//       if (filter != null && steps.length < 4) {
-//           steps.push({ "name": "location", "label": "Location" });
-//           p = 0.85;
-//       }
-//       else if (filter == null) {
-//           steps.pop();
-//           p = 0.75;
-//       }
-//       let datum = transformToGraph(routeDim.top(Infinity));
-//       renderSankey(datum);
-//       bindHover();
-//       renderLabels();
-//   });
-// }
+function selectParameters(stock_data, chart_object) {
+
+    const all_countries = Array.from([...new Set(stock_data.map(x => x.Destination))]).sort();
+    var countries_data = []
+    for (i = 0; i < all_countries.length; i++) {
+        countries_data.push({
+            country: all_countries[i]
+        })
+    }
+
+    let countrySelect = dc.selectMenu('#countries_bar_chart');
+    var ndx = crossfilter(countries_data);
+    var countryDimension = ndx.dimension(function(d) {
+        return d.country
+    });
+
+    countrySelect
+        .dimension(countryDimension)
+        .group(countryDimension.group())
+        .multiple(false)
+        .numberVisible(null)
+        .promptText('All Countries')
+        .promptValue(null);
+
+    const all_years = Array.from([...new Set(stock_data.map(x => x.Year))]).sort();
+    var years_data = []
+    for (i = 0; i < all_years.length; i++) {
+        years_data.push({
+            country: all_years[i]
+        })
+    }
+
+    let yearSelect = dc.selectMenu('#years_bar_chart');
+    var ndx = crossfilter(years_data);
+    var yearDimension = ndx.dimension(function(d) {
+        return d.country
+    });
+
+    yearSelect
+        .dimension(yearDimension)
+        .group(yearDimension.group())
+        .multiple(false)
+        .numberVisible(null)
+        .promptText('Year')
+        .promptValue(null);
+
+    dc.renderAll();
+
+    countrySelect.on('filtered', function(chart, filter) {
+        if (filter != null) {
+          // show selected country
+          console.log(filter)
+          // chart_object.setCountry(filter);
+        }
+        else {
+          // show all countries
+        }
+    });
+}
 
 
 whenDocumentLoaded(() => {
